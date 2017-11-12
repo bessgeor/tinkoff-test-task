4c6aff35dfec01f963628edc1399ce07d5c27c4a
bessgeor <bessgeor@gmail.com>
2017-11-12 22:52:19 +0300

    Injected mongo database as singleton

--- src/SkillazTestTask/Startup.cs
+++ src/SkillazTestTask/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System;
 
 namespace SkillazTestTask
 {
@@ -10,6 +12,10 @@ namespace SkillazTestTask
 		{
 			services.AddCors();
 			services.AddMvc();
+			MongoUrl url = new MongoUrl( Environment.GetEnvironmentVariable( "MONGODB_URI" ) );
+			IMongoClient client = new MongoClient( url );
+			IMongoDatabase database = client.GetDatabase( "heroku_8pg0s8ql" );
+			services.AddSingleton( database );
 		}
 
 		public void Configure( IApplicationBuilder app, IHostingEnvironment env )
